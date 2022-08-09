a42981a7b2d2a07ac080133e8b26a4d63ed1116a
Roman Shiryaev <roma78020@yandex.ru>
2022-08-09 12:34:07 +0700

    Решение задачи 4

--- Home Works/HW1/Program.cs
+++ Home Works/HW1/Program.cs
@@ -27,3 +27,27 @@ else
 }
 */
 
+/*
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+
+if (n2 > max) max = n2;
+if (n3 > max) max = n3;
+
+Console.Write($"Максимальное число = {max}");
+*/
+
