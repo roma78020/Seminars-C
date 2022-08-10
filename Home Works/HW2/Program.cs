a645b896ff85ffd13b75f58f8204e24a093ead21
Roman Shiryaev <roma78020@yandex.ru>
2022-08-10 18:57:21 +0700

    Решение дз 10

---
+++ Home Works/HW2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int FindSecondDigit(int number)
+{
+    int dec = (number % 100) / 10;
+    return dec;
+}
+
+Console.Write ("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondDigit = FindSecondDigit(number);
+
+Console.WriteLine ($"Вторая цифра числа {number} это {SecondDigit}");
+*/
+
