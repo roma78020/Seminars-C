3379792505f716e8b0d4cfe8a3b9a3f29f4191d6
Roman Shiryaev <roma78020@yandex.ru>
2022-08-10 20:12:59 +0700

    Решение дз 13

--- Home Works/HW2/Program.cs
+++ Home Works/HW2/Program.cs
@@ -19,3 +19,34 @@ int SecondDigit = FindSecondDigit(number);
 Console.WriteLine ($"Вторая цифра числа {number} это {SecondDigit}");
 */
 
+/*
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int FindThirdDigit(int number)
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+    
+    return number;
+}
+
+Console.Write ("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ThreeDigit = FindThirdDigit(number);
+
+if (ThreeDigit <= 99)
+    {
+        Console.WriteLine ($"У числа {number} нет третьей цифры");
+    }
+    else
+    {
+        ThreeDigit = ThreeDigit % 10;
+        Console.WriteLine ($"Третья цифра числа {number} это {ThreeDigit}");
+    }
+*/
