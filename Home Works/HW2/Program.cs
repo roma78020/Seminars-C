67061d3ecd8a83909ddb31420d07cfcc6bc76f1c
Roman Shiryaev <roma78020@yandex.ru>
2022-08-10 20:37:13 +0700

    Решение дз 15

--- Home Works/HW2/Program.cs
+++ Home Works/HW2/Program.cs
@@ -50,3 +50,26 @@ if (ThreeDigit <= 99)
         Console.WriteLine ($"Третья цифра числа {number} это {ThreeDigit}");
     }
 */
+
+/*
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+void FindWeekend (int number)
+{
+    if (number <= 5) 
+    Console.WriteLine ($"{number} день недели не является выходным");
+    else 
+    Console.WriteLine ($"{number} день недели ВЫХОДНОООЙ!УРААА!");
+}
+
+Console.Write ("Введите число дня недели (от 1 до 7): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 7)
+    FindWeekend(number);
+else
+    Console.WriteLine ($"Будь внимательней! Число {number} не является днем недели. Повтори попытку и введи число от 1 до 7.>");
+*/
