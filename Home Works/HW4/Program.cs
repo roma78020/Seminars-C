3d3d092fec095f32acd526300b875b09a9c78ce8
Roman Shiryaev <roma78020@yandex.ru>
2022-08-17 23:09:21 +0700

    Решение задачи 29

--- Home Works/HW4/Program.cs
+++ Home Works/HW4/Program.cs
@@ -51,3 +51,37 @@ int number = Convert.ToInt32 (Console.ReadLine());
 Console.WriteLine ($"Сумма цифр в числе {number} = {FindSumDigits (number)}");
 
 */
+
+/*
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int [] CreateRandomArray(int size)
+{
+    int[] newArray = new int [size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите элемент массива");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+        
+    }
+    return newArray;
+}
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    
+    Console.WriteLine();
+}
+
+Console.Write ("Введите длину массива: ");
+int a = Convert.ToInt32 (Console.ReadLine());
+
+int[] myArray = CreateRandomArray (a);
+ShowArray(myArray);
+
+*/
