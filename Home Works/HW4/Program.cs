3527574716ce6ebcaf40983d76d0df94094b543e
Roman Shiryaev <roma78020@yandex.ru>
2022-08-17 22:05:38 +0700

    Решение задачи 27

--- Home Works/HW4/Program.cs
+++ Home Works/HW4/Program.cs
@@ -2,9 +2,7 @@
 //Задача 25: Напишите цикл, который принимает
 //на вход два числа (A и B) и 
 //возводит число A в натуральную степень B.
-
 //3, 5 -> 243 (3⁵)
-
 //2, 4 -> 16
 
 void Exponentiation (int A,int B)
@@ -26,4 +24,30 @@ Console.Write ("Введите степень в которую возвести
 int B = Convert.ToInt32 (Console.ReadLine());
 
 Exponentiation(A,B);
+*/
+
+/*
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int FindSumDigits (int number)
+{
+    int ostatok = 0;
+    int sum = 0;
+    while (number > 0)
+    {
+        ostatok = number % 10;
+        sum = sum + ostatok;
+        number = number / 10;
+    }
+    return sum ;
+}
+
+Console.Write ("Введите число: ");
+int number = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine ($"Сумма цифр в числе {number} = {FindSumDigits (number)}");
+
 */
