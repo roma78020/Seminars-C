ab83c5e44327e8fdea41c2af71c74e7a1356f98e
Roman Shiryaev <roma78020@yandex.ru>
2022-08-24 03:38:09 +0700

    ДЗ.Задача 41 и 43

---
+++ Home Works/HW6/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 41:
+//  Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 
+//  0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+/*
+int FindPositiveNumbers (int[] newarray )
+{
+    int count=0;
+    for (int i = 0; i < newarray.Length; i++)
+    {
+    if (newarray[i]>0) count++;
+    }
+    return count;
+}    
+int [] CreateArray (int M)
+{
+    int [] array=new int[M]; 
+    for (int i = 0; i< M; i++)
+    {
+    Console.Write($"Введите {i+1} число: ");
+    int a = Convert.ToInt32(Console.ReadLine());  
+        array[i]=a;
+    }
+    return array;
+}
+ 
+Console.Write("Сколько чисел будите вводить?: ");
+int M = Convert.ToInt32(Console.ReadLine());  
+int[] myArray = CreateArray (M);
+int find =FindPositiveNumbers(myArray);
+Console.WriteLine(find);
+*/
+
+// Задача 43: Напишите программу,
+// которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+/*
+void FindIntersections(double b1, double k1, double b2, double k2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точкой пересечения будет ({x},{y})");
+}
+Console.Write("Введите первое значение первого отрезка: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе значение первого отрезка: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите первое значение второго отрезка: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе значение второго отрезка: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+FindIntersections(b1, k1, b2, k2);
+*/
