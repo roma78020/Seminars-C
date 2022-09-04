95d4e0c008cd2603c749d3951bc6b89da0881271
Roman Shiryaev <roma78020@yandex.ru>
2022-09-05 00:09:27 +0700

    ДЗ №68

--- Home Works/HW9/Program.cs
+++ Home Works/HW9/Program.cs
@@ -3,6 +3,7 @@
 // все натуральные числа в промежутке от M до N.
 // M = 1; N = 5. -> ""1, 2, 3, 4, 5""
 // M = 4; N = 8. -> ""4, 6, 7, 8""
+
 /*
 void ShowNumbers (int n,int m)
 {
@@ -20,10 +21,14 @@ void ShowNumbers (int n,int m)
         }
     }
     else
-        Console.WriteLine(n + " ");    
+        Console.Write(n + " ");    
 }
 
-ShowNumbers(5,20);
+Console.Write("Введите первое значение: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ShowNumbers(m,n);
 */
 
 
@@ -32,6 +37,7 @@ ShowNumbers(5,20);
 // элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
 /*
 int FindSum (int m,int n)
 {
@@ -49,11 +55,30 @@ int FindSum (int m,int n)
     return n;
 }
 
-Console.WriteLine(FindSum(1,15));
-
+Console.Write("Введите первое значение: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FindSum(m,n));
 */
 
+/*
 // Задача 68: Напишите программу вычисления
 // функции Аккермана с помощью рекурсии.
 // Даны два неотрицательных числа m и n.
-// m = 2, n = 3 -> A(m,n) = 29
+// m = 2, n = 3 -> A(m,n) = 9
+
+int FunctionAccermana (int m,int n)
+{
+    if (m==0) return n+1;
+    if (m > 0 && n==0) return FunctionAccermana (m-1,1);
+    if (m > 0 && n > 0) return FunctionAccermana (m-1,FunctionAccermana (m,n-1));
+    else return n+1;               
+}
+
+Console.Write("Введите первое значение: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(FunctionAccermana(m,n));
+*/
